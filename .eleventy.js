829c3ca2c174208b6ad4b58ab8c4490291452891
captain-pyo <captainpyo059@gmail.com>
2026-08-14 12:28:38 +0900

    Create .eleventy.js

---
+++ .eleventy.js
@@ -0,0 +1,14 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/admin");
+  eleventyConfig.addFilter("dateKr", function (dateObj) {
+    const d = new Date(dateObj);
+    return `${d.getFullYear()}.${String(d.getMonth() + 1).padStart(2, "0")}.${String(d.getDate()).padStart(2, "0")}`;
+  });
+  return {
+    dir: { input: "src", output: "_site", includes: "_includes", data: "_data" },
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk"
+  };
+};
