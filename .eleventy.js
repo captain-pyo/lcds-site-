b16c0a3f48ff578dced383ea2d39dc15c2060976
captain-pyo <captainpyo059@gmail.com>
2026-08-16 11:14:55 +0900

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,3 @@
-const yaml = require("js-yaml");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addDataExtension("yml", (contents) => yaml.load(contents));
@@ -8,6 +7,14 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("dateKr", function (dateObj) {
     const d = new Date(dateObj);
     return `${d.getFullYear()}.${String(d.getMonth() + 1).padStart(2, "0")}.${String(d.getDate()).padStart(2, "0")}`;
+  });
+  eleventyConfig.addFilter("groupByCourse", function (testimonialsObj) {
+    const groups = {};
+    Object.values(testimonialsObj || {}).forEach((t) => {
+      if (!groups[t.course]) groups[t.course] = [];
+      groups[t.course].push(t.quote);
+    });
+    return Object.keys(groups).map((course) => ({ course, quotes: groups[course] }));
   });
   return {
     dir: { input: "src", output: "_site", includes: "_includes", data: "_data" },
